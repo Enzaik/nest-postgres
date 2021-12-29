ba4c5e48c1341deda1bf2216dc7a735bbd018b81
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-12-29 18:45:05 +0100

    feat: add signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private usersRepository: UserRepository,
   ) {}
+
+  signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
