dacab029f7ae56680333cd750545678282d9f0bd
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-12-29 17:04:55 +0100

    feat: pg filter

--- src/tasks/dto/tasks.repository.ts
+++ src/tasks/dto/tasks.repository.ts
@@ -21,7 +21,17 @@ export class TasksRepository extends Repository<Task> {
   }
 
   async getTasks(filterDto: GetTasksFilterDto) {
+    const { status, search } = filterDto;
     const query = this.createQueryBuilder('task'); // task entity query
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+    if (search) {
+      query.andWhere(
+        'task.title LIKE :search OR task.description LIKE :search',
+        { search: `%${search}%` },
+      );
+    }
     const tasks = await query.getMany();
     return tasks;
   }
