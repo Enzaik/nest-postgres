7bd43673972f7740a1dd61bf2ac99c3fda4d5bfb
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-12-29 19:51:33 +0100

    feat: register jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './users.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: { expiresIn: 3600 },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
