f4f32b22423b22f62663f8e21d12870befbf722b
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-12-29 19:22:34 +0100

    feat: salthash password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,5 @@
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import {
   ConflictException,
@@ -11,9 +12,12 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
     try {
       await this.save(user);
