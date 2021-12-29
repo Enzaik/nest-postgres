36cbecfa02fb2828b7b09c703cc4ae6f1749edf9
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-12-29 11:54:51 +0100

    feat: add task not found validation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { v4 as uuid } from 'uuid';
@@ -47,7 +47,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+    return task;
   }
 
   deleteTask(id: string): string {
