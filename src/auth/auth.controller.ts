7120f7a1b41452bd954f2dce2cbec088c539f5a4
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-12-30 09:27:52 +0100

    feat: add test endpoint with token auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  //  Req, UseGuards
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
+// import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -16,4 +22,10 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     return await this.authService.signin(authCredentialsDto);
   }
+
+  // @Post('/test')
+  // @UseGuards(AuthGuard())
+  // test(@Req() req) {
+  //   console.log({ req });
+  // }
 }
