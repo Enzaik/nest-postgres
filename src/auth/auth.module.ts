3b3e941ad4cb8e6053a741f475b16cf7d15dbc84
Ernesto Alejandro Diaz Catala <ernestoalejandro.diazcatala@grandparade.co.uk>
2021-12-30 13:53:20 +0100

    add jwtsecret config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,19 @@ import { AuthService } from './auth.service';
 import { UsersRepository } from './users.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: { expiresIn: 3600 },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: 3600 },
+      }),
     }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
